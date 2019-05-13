bbf930e572f96201a6eb6bd617ac1ee1240f8299
Minhtality <Minhtality@Minhs-MacBook-Air.local>
2019-05-12 22:23:49 -0700

    converted to ejs for now

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 //connect to DB
 mongoose.connect('mongodb://localhost/Personal');
@@ -26,38 +27,69 @@ let Blog = require('./models/blog');
 app.use(express.static(path.join(__dirname,'public')));
 //Load View Engine
 app.set('Views',path.join(__dirname,'Views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 //Home Route
 app.get('/',function(req, res){
   res.render('home');
 });
 
-app.get('/*', function(req, res){
+app.get('/*',function(req, res){
   res.render('home');
 });
 
 // //WIP Route
-// app.get('/hidden',function(req, res){
+app.get('/allBlogs', function(req, res){
+  res.render('allBlogs');
+});
+// app.get('/allBlogs',function(req, res){
 //   Blog.find({},function(err, blogs){
 //     if(err){
 //       console.log(err);
 //     }else {
-//       res.render('index',{
+//       res.render('Blog',{
 //         title:'Blogs',
 //         blogs: blogs
 //       });
 //     }
 //   });
 // });
-//
-//
-// //Add Route
-// app.get('/blogs/add', function(req,res){
-//   res.render('add_blog',{
-//     title:'Add blogs'
-//   });
-// });
+
+//get blog by id
+app.get('/blogs/:id', function(req, res){
+  Blog.findById(req.params.id, function(err, blogs){
+    res.render('viewBlog',{
+      blogs:blogs
+    });
+  });
+});
+
+//Add Route
+app.get('/blogs/add', function(req, res){
+  res.render('blog_add',{
+    title:'Add blogs'
+  });
+});
+//POST Route
+app.post('/blogs/add', function(req, res){
+  let blog = new Blog();
+  blog.title = req.body.title;
+  blog.author = req.body.author;
+  blog.body = req.body.body;
+  blog.save(function(err){
+    if(err){
+      console.log(err);
+      return;
+    }else {;
+      res.redirect('/blogs');
+    }
+  })
+});
 
 
 
